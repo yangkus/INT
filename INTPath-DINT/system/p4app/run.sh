d0a1e221d816f95f3fe62708e975aa8a4b4ca52f
yangkus <78351681+yangkus@users.noreply.github.com>
2024-04-07 19:13:26 +0800

    Delete INTPath-DINT directory

--- INTPath-DINT/system/p4app/run.sh
+++
@@ -1,7 +0,0 @@
-p4c --target bmv2 --arch v1model --std p4-16 -o ./ ./app.p4
-p4c --target bmv2 --arch v1model --std p4-16 -o ./ ./dump_app.p4
-p4c --target bmv2 --arch v1model --std p4-16 -o ./ ./dint_app.p4
-p4c --target bmv2 --arch v1model --std p4-16 -o ./ ./dint_app2.p4
-p4c --target bmv2 --arch v1model --std p4-16 -o ./ ./dint_app3.p4
-p4c --target bmv2 --arch v1model --std p4-16 -o ./ ./dint_app4.p4
-p4c --target bmv2 --arch v1model --std p4-16 -o ./ ./simple_dint_app.p4
