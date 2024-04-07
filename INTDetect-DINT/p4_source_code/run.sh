48ee2812d573213e039057385a6419df4c2eb49c
yangkus <78351681+yangkus@users.noreply.github.com>
2024-04-07 19:13:14 +0800

    Delete INTDetect-DINT directory

--- INTDetect-DINT/p4_source_code/run.sh
+++
@@ -1,2 +0,0 @@
-#p4c-bm2-ss --p4v 16 -o my_int.json ./my_int.p4
-p4c --target bmv2 --arch v1model --std p4-16 -o ./ ./my_int.p4
--- INTDetect-DINT/setup_mininet.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sudo python3 topology/clos.py --behavioral-exe $HOME/behavioral-model/targets/simple_switch/simple_switch --json p4_source_code/my_int.json
